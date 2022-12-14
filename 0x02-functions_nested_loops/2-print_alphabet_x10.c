6b0006bb90f4d41889f3499844021e0894b601d7
Tess <tesstera34@gmail.com>
2022-12-14 13:52:06 -0800

    ten

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,3 +1,16 @@
 #include "main.h"
+/**
+ * print_alphabet_x10- print alphabet ten times
+ */
 void print_alphabet_x10(void);
+{
+	int n;
+	char ch;
+
+	for (n = 0; n <= 9; n++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+			_putchar('ch');
+		_putchar('\n')
+	}
 
