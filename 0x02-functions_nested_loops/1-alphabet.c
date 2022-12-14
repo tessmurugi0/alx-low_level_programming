e7c0832d9598266f0bcb33b24c3376cd1a68bef6
Tess <tesstera34@gmail.com>
2022-12-14 13:36:11 -0800

    alpha

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet- print lowercase alphabet
+ */
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+	_putchar(ch);
+	}
+	_putchar('\n');
+}
+
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,3 @@
+#include "main.h"
+void print_alphabet_x10(void);
+
