a8efb35df4acb3238e2cecac4ccc57156724c2db
Tess <tesstera34@gmail.com>
2022-12-14 14:38:21 -0800

    lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _islower- print lowercase
+ * @c- character
+ * Return: 1 if lowercase, 0 if not
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else 
+		return (0);
+}
