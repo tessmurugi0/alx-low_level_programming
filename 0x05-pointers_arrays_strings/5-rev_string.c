b8d112b354902691ce9fb817ec496ab8ecdb0744
Tess <tesstera34@gmail.com>
2022-12-20 12:56:04 -0800

    rev str

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -13,26 +13,16 @@ void rev_string(char *s)
 
 		int a = 0, b = 0;
 
-			char c;
+		char c;
+			
+		while (s[a] != '\0')
+			a++;
 
+		while (b < a--)
 
-
-				while (s[a] != '\0')
-
-							a++;
-
-
-
-					while (b < a--)
-
-							{
-
-										c = s[b];
-
-												s[b++] = s[a];
-
-														s[a] = c;
-
-															}
-
+		{
+			c = s[b];
+		 	s[b++] = s[a];
+			s[a] = c;
+		}
 }
