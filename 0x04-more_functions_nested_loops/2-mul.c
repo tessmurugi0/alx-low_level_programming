e23a4dcebb4c236329e9b648cbf2da28f9fc17ef
Tess <tesstera34@gmail.com>
2022-12-23 18:29:37 -0800

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul-checks for integer
+ * @a: variable
+ * @b: variable
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+	return (mul);
+}
