8645f0e2918488104b893d26033566a4c36df8de
Tess <tesstera34@gmail.com>
2023-05-28 12:21:42 -0700

    4-isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * _isalpha - checks for alphabets
+ * Description: uses isalpha to check fot alphabet
+ * @c: character to check
+ * Return: Success 1 else 0.
+ */
+int _isalpha(int c)
+{
+	if (_isalpha(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
