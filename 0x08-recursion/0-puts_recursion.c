1c490d82e76ffc4b68d3cc253c1dd2dd72c96382
Tess <tesstera34@gmail.com>
2023-06-07 04:03:18 -0700

    0-puts_recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *  _puts_recursion - prints a string
+ *  @s: character used
+ *  Return: Nothing
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		puts(" ");
+		return;
+	}
+	putchar(*s);
+	_puts_recursion(s + 1);
+}
