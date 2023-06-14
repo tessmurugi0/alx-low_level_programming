526948fe799834699a9c29a932bc1ccf75512908
Tess <tesstera34@gmail.com>
2023-06-14 02:20:23 -0700

    whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * argv - pointer to the strings
+ * argc - counts arguments
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
