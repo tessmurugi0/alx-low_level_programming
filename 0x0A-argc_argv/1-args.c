65b79b05fdfc19877bef82241f9832fc1b2490cc
Tess <tesstera34@gmail.com>
2023-06-15 00:08:10 -0700

    nargs

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * argc - count arguments
+ * argv - arrays to pointers
+ * Return: Always 0.
+ */
+int main(int argc, __attribute__((unused))  char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
