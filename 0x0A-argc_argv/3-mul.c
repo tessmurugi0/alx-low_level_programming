abcf496fac484768eff44b27d80411177007df0b
Tess <tesstera34@gmail.com>
2023-06-16 01:30:58 -0700

    mul

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - mulitipies 2 numbers
+ * @argv: array of [ointers
+ * @argc: count arguements
+ * Return: 1 Fail 0 Succes
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error");
+		return (1);
+	}
+	else
+	{
+		printf("%d\n", (atoi(argv[1]) * atoi(argv[2])));
+		return (0);
+	}
+}
